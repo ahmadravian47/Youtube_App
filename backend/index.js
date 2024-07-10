5e9e643afae6514ac68f37eb6499f27265d9f62f
Ahmad Touseef <155019192+ahmadravian47@users.noreply.github.com>
2024-07-10 10:57:27 +0500

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -35,7 +35,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cors(
   {
-    origin: ["https://tubex1.netlify.app"],
+    origin: ["http://localhost:5173"],
     methods: ["POST", "GET"],
     credentials: true
   }
